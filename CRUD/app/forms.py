764d72b8e3e58537d225d06dddfa1cc33b06a75a
deivisonmacharete <107815532+deivisonmacharete@users.noreply.github.com>
2023-02-08 22:25:48 -0300

    Delete CRUD directory

--- CRUD/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from app.models import Clientes
-
-# Create the form class.
-class ClientesForm(ModelForm):
-     class Meta:
-        model = Clientes
-        fields = ['Nome', 'Email', 'Telefone', 'Senha']
